0823a7f05fae0ec02180547ba3cfde8e44f74d87
Leo Irakliotis <lgreco@gmail.com>
2020-02-12 14:59:18 -0600

    Reformatting Airplane Class

--- source/COMP170/codeExamples/Flying/Airplane.java
+++ source/COMP170/codeExamples/Flying/Airplane.java
@@ -1,15 +1,4 @@
-
-Review of Week 5
-=================
-
-Our first formal introduction to Classes and objects is `available from the Github repository <https://github.com/lgreco/cdp/blob/master/source/COMP170/codeExamples/Person.java>`_. To download the file, open it in Raw view, then right-click and Save As.
-
-We saw how to define a Person class in the example above. Next we worked on a more intricate example, defining a class for airplanes as follows:
-
-.. code-block:: java
-   :linenos:
-   
-   public class Airplane {
+public class Airplane {
 
     private String tailNumber;
     private String make;
@@ -48,5 +37,4 @@ We saw how to define a Person class in the example above. Next we worked on a mo
 
         System.out.println(myFavAirplane.sayAltitude());
     }
-  }
-  
+}
