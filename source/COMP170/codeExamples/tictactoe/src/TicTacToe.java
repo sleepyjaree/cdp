503d3587f8eedb8d605dec25c574ac2f048e29b8
Leo Irakliotis <lgreco@gmail.com>
2020-03-20 10:47:49 -0500

    COMP 170 tic tac toe

---
+++ source/COMP170/codeExamples/tictactoe/src/TicTacToe.java
@@ -0,0 +1,25 @@
+public class TicTacToe {
+
+    private static final int SIZE = 3;
+    private static final char X = 'x'; // x marks
+    private static final char O = 'o'; // o marks
+    private static final char E = '-'; // - for empty squares
+
+    private char[][] gameboard = new char[SIZE][SIZE];
+
+    /**
+     * Set up a clean game board. Set all squares to empty
+     */
+    public void setUpGame () {}
+
+    /**
+     * Display the board
+     */
+    public void displayBoard() {}
+
+    /**
+     * Controls the game.
+     */
+    public void play() {}
+
+}
