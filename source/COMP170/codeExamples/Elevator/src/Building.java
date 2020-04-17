cc32cc62d33743f31a65c0c27290f99a7d7bdd5a
Leo Irakliotis <lgreco@gmail.com>
2020-04-17 12:48:37 -0500

    COMP 170 Fri 17 APR Building class

--- source/COMP170/codeExamples/Elevator/src/Building.java
+++ source/COMP170/codeExamples/Elevator/src/Building.java
@@ -9,7 +9,19 @@ public class Building {
         } else {
             // put a floor on top of the highest floor in the building
             // ... ok, but HOW do you find the highest floor
+            Floor highest = highestFloor(building);
+            int hightestFloorNumber = highest.getFloorNumber();
+            int newFloorNumber = 1+hightestFloorNumber;
+            Floor newFloor = new Floor();
         }
+    }
+
+    public Floor highestFloor(Building building) {
+        Floor current = building.ground;
+        while (current.above != null) {
+            current = current.above;
+        }
+        return current;
     }
 
     public int countFloors() { return 1;}
@@ -30,6 +42,7 @@ public class Building {
             below = null;
         }
 
+        public void setFloorNumber(int floorNumber) { this.floorNumber = floorNumber; }
         public void setFloorName(String floorName) { this.floorName = floorName;}
         public void setAbove(Floor above) { this.above = above;}
         public void setBelow(Floor below) { this.below = below;}
