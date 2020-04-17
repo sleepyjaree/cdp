c5f3b1104aade409f42d3787351c178a77714657
Leo Irakliotis <lgreco@gmail.com>
2020-04-17 12:38:30 -0500

    COMP 170 Fri 17 APR Building class

--- source/COMP170/codeExamples/Elevator/src/Building.java
+++ source/COMP170/codeExamples/Elevator/src/Building.java
@@ -1,61 +1,32 @@
+
 public class Building {
 
-   Floor ground = new Floor();
+    Floor ground = null;
 
-    /**
-     * default constructor
-     */
-    public Building() {
-    } // default constructor
+    public void addFloor(Building building) {}
 
+    public int countFloors() { return 1;}
 
-    public static Floor lastFloor(Building building) {
-        Floor current = building.ground;
-        while (current.above != null) {
-            current = current.above;
-        }
-        return current;
-    }
+    
 
-    public static Building addFloor(Building building) {
-        Floor currentLastFloor = lastFloor(building);
-        int newFloorNumber = 1 + currentLastFloor.getFloorNumber();
-        String newFloorName = "** "+newFloorNumber+" **";
-        Floor floorToAdd = new Floor(newFloorName, newFloorNumber);
-        currentLastFloor.above = floorToAdd;
-        floorToAdd.below = currentLastFloor; // potential bug .. when at ground
-    }
+    private class Floor {
 
-    static class Floor {
-        private String floorName;
-        private int floorNumber;
+        int floorNumber;
+        String floorName;
         Floor above;
         Floor below;
-        Floor() {
+
+        public Floor() {
             floorNumber = 0;
-            floorName = "Ground";
+            floorName = "N/A";
             above = null;
             below = null;
         }
-        Floor(String floorName, int floorNumber) {
-            this.floorName = floorName;
-            this.floorNumber = floorNumber;
-        }
-        void setFloor(String floorName, int floorNumber) {
-            this.floorName = floorName;
-            this.floorNumber = floorNumber;
-        }
-        void setFloor (String floorName, int floorNumber, Floor above, Floor below) {
-            this.floorName = floorName;
-            this.floorNumber = floorNumber;
-            this.above = above;
-            this.below = below;
-        }
-        void setAbove(Floor above) { this.above = above; }
-        void setBelow(Floor below) { this.below = below; }
-        String getFloorName() { return floorName; }
-        String getAbove() { return above; }
-        String getBelow() { return below; }
-        int getFloorNumber() { return floorNumber; }
+
+        public void setFloorName(String floorName) { this.floorName = floorName;}
+        public void setAbove(Floor above) { this.above = above;}
+        public void setBelow(Floor below) { this.below = below;}
+
+        public int getFloorNumber() { return floorNumber; }
     }
 }
