2e2be60b08d18e6143c6d79abfe0d53fd9ddbcce
Leo Irakliotis <lgreco@gmail.com>
2020-04-19 12:02:54 -0500

    COMP 170 Building as a linked list

--- source/COMP170/codeExamples/Elevator/src/Building.java
+++ source/COMP170/codeExamples/Elevator/src/Building.java
@@ -9,9 +9,11 @@
  */
 
 public class Building {
+
     /**
      * We assume that all buildings start from a ground floor; ie no basements!
      */
+
     private Floor ground;
 
     /**
@@ -21,6 +23,7 @@ public class Building {
      * rest is done with direct access to the Floor's fields from outside the class.
      * And a floorString method facilitates visualization, later in the program.
      */
+
     static class Floor {
         int floorNumber;
         Floor above, below;
@@ -48,33 +51,49 @@ public class Building {
      * @param ourBuilding the building to expand
      * @return the expanded building with one more floor added on the top
      */
+
     public static Building addFloor(Building ourBuilding) {
+
         // Get a new floor object ready. We use the default constructor for Floor
         // that gives us a minimal object, whose fields will be set to the
         // appropriate values in the following few lines of code.
+
         Floor newFloor = new Floor();
+
         // Determine where to attach this new floor to.
+
         if ( ourBuilding.ground == null ) {
+
             // If the ground floor has not be completed, make the new floor
             // object, the ground floor. And since this is now the only existing
             // floor of the building, its above and below points remain null
             // (the default constructor for Floor, took care of that).
+
             ourBuilding.ground = newFloor;
+
         } else {
+
             // ourBuilding has a ground floor, so the new floor we are adding
             // needs to go to the top of the building. Method highestFloor
             // finds the highest existing floor. And then we can obtain its
             // floor number as well.
+
             Floor highestExistingFloor = ourBuilding.highestFloor();
             int highestExistingFloorNumber = highestExistingFloor.floorNumber;
+
             // The floor number for the new floor we are adding to the building
             // is the floor number of the highest existing floor, increased by 1.
+
             newFloor.floorNumber = highestExistingFloorNumber+1;
+
             // The pointer above the highest existing floor which was null until now,
             // becomes the new floor we just added.
+
             highestExistingFloor.above = newFloor;
+
             // And the floor below the new floor we just added, is the floor that
             // until now was the highest existing floor.
+
             newFloor.below = highestExistingFloor;
         }
         return ourBuilding;
@@ -87,12 +106,15 @@ public class Building {
      * floor above it).
      * @return First floor object with a null .above pointer.
      */
+
     public Floor highestFloor() {
         Floor current = ground;
         while (current.above != null)
+
             // If the current floor has a pointer to a floor above,
             // make the floor above the current floor and repeat.
             // (Notice, No {} after while() if there is only 1 line to process )
+
             current = current.above;
         return current;
     } // method highestFloor
@@ -101,6 +123,7 @@ public class Building {
      * Method traverseBuilding creates a simple visual display of a building.
      * @param thisBuilding is the object to visualize.
      */
+
     public static void traverseBuilding(Building thisBuilding) {
         Floor current = thisBuilding.ground;
         if (current == null ) {
